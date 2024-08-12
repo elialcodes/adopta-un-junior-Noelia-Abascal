652854bdc29991887dbcbe004cb5041edbab1748
Noelia <abascalcabreran@gmail.com>
2024-08-12 16:30:46 +0200

    logic with likes array fixed

--- src/hooks/useLikes.ts
+++ src/hooks/useLikes.ts
@@ -3,12 +3,13 @@ import { MOTIVES } from '../assets/data';
 
 export function useLikes() {
   const [likes, setLikes] = useState<boolean[]>(MOTIVES.map(() => false));
-
+  console.log(likes);
   const totalLikes = likes.filter((v) => v).length;
+  console.log(totalLikes);
 
   const handleLikedReason = useCallback((index: number) => {
     setLikes((prevLikes) =>
-      prevLikes.map((like, i) => (i === index ? !like : like)),
+      prevLikes.map((like, i) => (i + 1 === index ? !like : like)),
     );
   }, []);
 
